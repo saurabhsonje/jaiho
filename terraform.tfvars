2be19cb191b57f80074d444d668647d7a479fced
saurabhsonje <50398982+saurabhsonje@users.noreply.github.com>
2020-05-23 15:05:15 +0530

    Add files via upload

---
+++ terraform.tfvars
@@ -0,0 +1,8 @@
+# Azure Subscription Id
+azure-subscription-id = "cf97669a-b8a4-471e-b5b7-5f96bf875432"
+# Azure Client Id/appId
+azure-client-id = "cd00171f-e88e-49e2-9b87-8fdc6c71d776"
+# Azure Client Secret/password
+azure-client-secret = "4ed3bcfe-0969-434d-9afc-8f59ac226938"
+# Azure Tenant Id
+azure-tenant-id = "c160a942-c869-429f-8a96-f8c8296d57db"
---
+++ varible.tf
@@ -0,0 +1,20 @@
+# Azure Subscription Id
+variable "azure_subscription_id" {
+  //type        = string 
+  description = "Azure Subscription Id"
+}
+# Azure Client Id/appId
+variable "azure_client_id" {
+ // type        = string
+  description = "Azure Client Id/appId"
+}
+# Azure Client Id/appId
+variable "azure_client_secret" {
+ // type        = string
+  description = "Azure Client Id/appId"
+}
+# Azure Tenant Id
+variable "azure_tenant_id" {
+  //type        = string
+  description = "Azure Tenant Id"
+}
