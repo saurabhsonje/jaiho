34e4f516aa56b2213baed8c726cf4c4fbaf24ddb
saurabhsonje <50398982+saurabhsonje@users.noreply.github.com>
2020-06-01 23:45:42 +0530

    Update storage.tf

--- backend-azurerm/storage.tf
+++ backend-azurerm/storage.tf
@@ -1,11 +1,11 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "DAL"
-    storage_account_name = "azurermstorage"
-    container_name       = "azurermcontainer"
+    resource_group_name  = "SAURABH"
+    storage_account_name = "azureazstorage"
+    container_name       = "azureazcontainer"
     key                  = "terraform.tfstate"
     use_msi              = true
-    subscription_id  = "cf97669a-b8a4-471e-b5b7-5f96bf875432"
+    subscription_id  = "c7a4cb49-a08b-453e-82ad-c36c75247cf9"
     tenant_id        = "c160a942-c869-429f-8a96-f8c8296d57db"
   }
 }
